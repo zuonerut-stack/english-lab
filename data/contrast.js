6001cb32babb763039bd6a6f72d9bf2488f07f47
zuonerut-stack <zuonerut@gmail.com>
2026-03-16 19:35:24 +0900

    Update contrast.js

--- data/contrast.js
+++ data/contrast.js
@@ -3,238 +3,238 @@ const contrastData=[
 {
 sentence:"One sandwitch for lunch usually suffices for you, but for me it is not enough.",
 target:"suffices"
-clue:"enough"
+,clue:"enough"
 },
 
 {
 sentence:"Don't digress and stick to the topic.",
 target:"digress"
-clue:"stick to"
+,clue:"stick to"
 },
 
 {
 sentence:"Your account of the fight concurs with Joanne's but differs from the accounts given by the other witnesses.",
 target:"concurs"
-clue:"differs"
+,clue:"differs"
 },
 
 {
 sentence:"Roses in bloom are a common sight in summer, but a rarity in late Nov.",
 target:"common sight"
-clue:"rarity"
+,clue:"rarity"
 },
 
 {
 sentence:" The tables in the restaurant were all occupied, and we waited more than ten minutes for one to become vacant.",
 target:"occupied"
-clue:"vacant"
+,clue:"vacant"
 },
 
 {
 sentence:"There are few theaters here, but on Broadway there are theaters galore.",
 target:"galore"
-clue:"few"
+,clue:"few"
 },
 {
 sentence:" I do not shrink from this responsibility; I welcome it.",
 target:"shrink"
-clue:"welcome"
+,clue:"welcome"
 },
 {
 sentence:"A bank teller can usually tell the difference between genuine $100 bills and counterfeit ones.",
 target:"counterfeit"
-clue:"genuine"
+,clue:"genuine"
 },
 {
 sentence:"I come to bury Caesar, not to praise him. The evil that men do lives after them; The good is oft interred with their bones; So let it be with Caesar.",
 target:"interred"
-clue:"live after"
+,clue:"live after"
 },
 {
 sentence:"I can't understand why Terry has become so hostile to me. We always have been friends.",
 target:"hostile"
-clue:"have been friends"
+,clue:"have been friends"
 },
 {
 sentence:"When someone asked how many times we should write each spelling word, she said five times would be adequate.",
 target:"adequate"
-clue:"many times"
+,clue:"many times"
 },
 {
 sentence:"Not a single civilian was appointed to the dictator's cabinet. All the posts were given to military officers. ",
 target:"appointed"
-clue:"posts"
+,clue:"posts"
 },
 {
 sentence:"Then such a scramble as there is to get aboard, and to get ashore, and to take in freight and to discharge freight! ",
 target:"discharge"
-clue:"take in"
+,clue:"take in"
 },
 {
 sentence:"The owner is selling his gas station becuaes the profit is too small. He hopes to go into a more lucrative business.",
 target:"lucrative"
-clue:"profit = small"
+,clue:"profit = small"
 },
 {
 sentence:"Debbie, who has come late to every meeting, surprised us today by being punctual.",
 target:"punctual"
-clue:"come late"
+,clue:"come late"
 },
 {
 sentence:" As I hurried to the board, I inadvertently stepped on Alan's foot, but he thinks I did it on purpose.",
 target:"inadvertently"
-clue:"on purpose"
+,clue:"on purpose"
 },
 {
 sentence:"When you chair a discussion, it's unfair to call only on your friends. To be equitable, you should call on all who wish to speak, without favoritism.",
 target:"equitable"
-clue:"favoritism"
+,clue:"favoritism"
 },
 {
 sentence:"The only extemporaneous talk was Jerry's; all the other candidates gave memoritzed speeches.",
 target:"extemporaneous"
-clue:"memorized"
+,clue:"memorized"
 },
 {
 sentence:"If you agree, write 'yes'; if you dissent, write 'no'.",
 target:"dissent"
-clue:"agree"
+,clue:"agree"
 },
 {
 sentence:"A child trying to squeeze through the iron fence became stuck between two bars, but luckily whe was able to extricate herself.",
 target:"extricate"
-clue:"become stuck"
+,clue:"become stuck"
 },
 {
 sentence:"At first I was blamed for damaging Dad's computer, but when my sister said she was reponsible, I was exonerated.",
 target:"exonerated"
-clue:"be blamed for"
+,clue:"be blamed for"
 },
 {
 sentence:"Parking on our side of the street is prohibited on weekdays between 4 P.M. and 7 P.M. but permitted at all other times.",
 target:"permitted"
-clue:"prohibited"
+,clue:"prohibited"
 },
 {
 sentence:"If we can begin the meeting on time, we should be able to complete business and adjourn by 4:30 P.M.",
 target:"adjourn"
-clue:"begin"
+,clue:"begin"
 },
 {
 sentence:"Before the new hotel can be constructed, the two old buildings now on the site will have to be demolished.",
 target:"demolished"
-clue:"constructed"
+,clue:"constructed"
 }
 /*
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 {
 sentence:"",
 target:""
-clue:""
+,clue:""
 },
 */
 
