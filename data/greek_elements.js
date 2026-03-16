893855edcade1a60f0b7393970e46ac6ed34fc11
zuonerut-stack <zuonerut@gmail.com>
2026-03-17 00:21:55 +0900

    Update greek_elements.js

--- data/greek_elements.js
+++ data/greek_elements.js
@@ -1,1 +1,596 @@
+window.greeksData = [
+  {
+authentic: {
+  domain: "AUT",
+  synonyms: ["true","trustworthy","genuine","reliable"]
+},
 
+autobiography: {
+  domain: "AUT",
+  synonyms: []
+},
+
+autocrat: {
+  domain: "AUT",
+  synonyms: ["despot","dictator","tyrant"]
+},
+
+autograph: {
+  domain: "AUT",
+  synonyms: ["signature"]
+},
+
+automatic: {
+  domain: "AUT",
+  synonyms: ["self regulating","mechanized"]
+},
+
+automaton: {
+  domain: "AUT",
+  synonyms: []
+},
+
+autonomous: {
+  domain: "AUT",
+  synonyms: ["self governed","independent","sovereign"]
+},
+
+autonomy: {
+  domain: "AUT",
+  synonyms: ["sovereignty"]
+},
+
+autopsy: {
+  domain: "AUT",
+  synonyms: []
+},
+aristocracy: {
+  domain: "CRACY",
+  synonyms: []
+},
+
+autocracy: {
+  domain: "CRACY",
+  synonyms: []
+},
+
+bureaucracy: {
+  domain: "CRACY",
+  synonyms: []
+},
+
+democracy: {
+  domain: "CRACY",
+  synonyms: []
+},
+
+plutocracy: {
+  domain: "CRACY",
+  synonyms: []
+},
+
+technocracy: {
+  domain: "CRACY",
+  synonyms: []
+},
+
+Democrat: {
+  domain: "CRACY",
+  synonyms: []
+},
+demagogue: {
+  domain: "DEM",
+  synonyms: ["rabble-rouser"]
+},
+
+epidemic: {
+  domain: "DEM",
+  synonyms: ["widespread","rife"]
+},
+
+demarchy: {
+  domain: "DEM",
+  synonyms: []
+},
+
+demography: {
+  domain: "DEM",
+  synonyms: []
+},
+
+demotic: {
+  domain: "DEM",
+  synonyms: ["colloquial","popular"]
+},
+
+demiurge: {
+  domain: "DEM",
+  synonyms: []
+},
+panacea: {
+  domain: "PAN",
+  synonyms: ["cure-all","elixir"]
+},
+
+pandemonium: {
+  domain: "PAN",
+  synonyms: ["disorder","tumult","racket","uproar"]
+},
+
+panoply: {
+  domain: "PAN",
+  synonyms: []
+},
+
+panorama: {
+  domain: "PAN",
+  synonyms: []
+},
+
+pantomime: {
+  domain: "PAN",
+  synonyms: []
+},
+anachronism: {
+  domain: "CHRON",
+  synonyms: []
+},
+
+chronicle: {
+  domain: "CHRON",
+  synonyms: ["annals","history"]
+},
+
+chronological: {
+  domain: "CHRON",
+  synonyms: ["in sequence"]
+},
+
+synchronize: {
+  domain: "CHRON",
+  synonyms: []
+},
+
+isochronous: {
+  domain: "CHRON",
+  synonyms: []
+},
+kleptomania: {
+  domain: "MANIA",
+  synonyms: []
+},
+
+maniac: {
+  domain: "MANIA",
+  synonyms: ["insanity","excessive fondness"]
+},
+encyclopedia: {
+  domain: "PED",
+  synonyms: []
+},
+
+orthopedic: {
+  domain: "PED",
+  synonyms: []
+},
+
+pedagogue: {
+  domain: "PED",
+  synonyms: ["teacher"]
+},
+
+pedagogy: {
+  domain: "PED",
+  synonyms: []
+},
+
+pediatrics: {
+  domain: "PED",
+  synonyms: []
+},
+
+pedology: {
+  domain: "PED",
+  synonyms: []
+},
+
+pedantic: {
+  domain: "PED",
+  synonyms: ["scrupulous","precise"]
+},
+orthodontist: {
+  domain: "ORTHO",
+  synonyms: []
+},
+
+orthodox: {
+  domain: "ORTHO",
+  synonyms: ["accepted","conventional","approved","conservative"]
+},
+
+orthography: {
+  domain: "ORTHO",
+  synonyms: ["spelling"]
+},
+
+orthopedist: {
+  domain: "ORTHO",
+  synonyms: []
+},
+
+unorthodox: {
+  domain: "ORTHO",
+  synonyms: ["unconventional","heretical"]
+},
+genealogy: {
+  domain: "GENEA",
+  synonyms: ["lineage","pedigree"]
+},
+
+genesis: {
+  domain: "GENEA",
+  synonyms: ["birth","origin"]
+},
+
+heterogeneous: {
+  domain: "GENEA",
+  synonyms: ["dissimilar","varied","diverse"]
+},
+
+homogeneous: {
+  domain: "GENEA",
+  synonyms: ["similar","uniform"]
+},
+
+homogenize: {
+  domain: "GENEA",
+  synonyms: ["merge","blend","unite"]
+},
+
+barometer: {
+  domain: "MET",
+  synonyms: []
+},
+
+diameter: {
+  domain: "MET",
+  synonyms: []
+},
+
+odometer: {
+  domain: "MET",
+  synonyms: []
+},
+
+photometer: {
+  domain: "MET",
+  synonyms: []
+},
+
+speedometer: {
+  domain: "MET",
+  synonyms: []
+},
+
+symmetry: {
+  domain: "MET",
+  synonyms: []
+},
+
+tachometer: {
+  domain: "MET",
+  synonyms: []
+},
+antagonist: {
+  domain: "ANT",
+  synonyms: ["opponent","adversary","foe"]
+},
+
+antibiotic: {
+  domain: "ANT",
+  synonyms: []
+},
+
+antibody: {
+  domain: "ANT",
+  synonyms: []
+},
+
+antidote: {
+  domain: "ANT",
+  synonyms: ["antitoxin","antiserum","countermeasure"]
+},
+
+antipathy: {
+  domain: "ANT",
+  synonyms: ["distaste","repugnance","dislike","enmity"]
+},
+
+antiseptic: {
+  domain: "ANT",
+  synonyms: ["germicide"]
+},
+
+antonym: {
+  domain: "ANT",
+  synonyms: ["opposite word"]
+},
+acronym: {
+  domain: "ONOM",
+  synonyms: ["abbreviation"]
+},
+
+anonymous: {
+  domain: "ONOM",
+  synonyms: ["nameless","unidentified"]
+},
+
+homonym: {
+  domain: "ONOM",
+  synonyms: []
+},
+
+onomatopoeia: {
+  domain: "ONOM",
+  synonyms: []
+},
+
+pseudonym: {
+  domain: "ONOM",
+  synonyms: ["pen name","alias"]
+},
+
+synonym: {
+  domain: "ONOM",
+  synonyms: []
+},
+dermatologist: {
+  domain: "DERM",
+  synonyms: []
+},
+
+dermis: {
+  domain: "DERM",
+  synonyms: []
+},
+
+epidermis: {
+  domain: "DERM",
+  synonyms: []
+},
+
+hypodermic: {
+  domain: "DERM",
+  synonyms: []
+},
+
+taxidermist: {
+  domain: "DERM",
+  synonyms: []
+},
+agronomy: {
+  domain: "NOM",
+  synonyms: ["husbandry"]
+},
+
+astronomical: {
+  domain: "NOM",
+  synonyms: []
+},
+
+economic: {
+  domain: "NOM",
+  synonyms: ["profitable"]
+},
+
+gastronome: {
+  domain: "NOM",
+  synonyms: ["gourmet","epicure"]
+},
+
+nemesis: {
+  domain: "NOM",
+  synonyms: ["archenemy","rival"]
+},
+cellophane: {
+  domain: "PHAN",
+  synonyms: []
+},
+
+diaphanous: {
+  domain: "PHAN",
+  synonyms: ["sheer","transparent"]
+},
+
+fancy: {
+  domain: "PHAN",
+  synonyms: ["illusion","imagination","phantom"]
+},
+
+fantasy: {
+  domain: "PHAN",
+  synonyms: ["illusory image","imagination"]
+},
+
+phantom: {
+  domain: "PHAN",
+  synonyms: ["apparition","ghost","specter"]
+},
+
+phenomenal: {
+  domain: "PHAN",
+  synonyms: ["extraordinary","remarkable","exceptional"]
+},
+
+phenomenon: {
+  domain: "PHAN",
+  synonyms: ["occurrence","event","situation","prodigy"]
+},
+diathermy: {
+  domain: "THER",
+  synonyms: []
+},
+
+thermal: {
+  domain: "THER",
+  synonyms: []
+},
+
+thermonuclear: {
+  domain: "THER",
+  synonyms: []
+},
+
+thermostat: {
+  domain: "THER",
+  synonyms: []
+},
+protagonist: {
+  domain: "PROTO",
+  synonyms: ["main character"]
+},
+
+protocol: {
+  domain: "PROTO",
+  synonyms: ["treaty"]
+},
+
+protoplasm: {
+  domain: "PROTO",
+  synonyms: []
+},
+
+prototype: {
+  domain: "PROTO",
+  synonyms: ["original model"]
+},
+
+protozoan: {
+  domain: "PROTO",
+  synonyms: []
+},
+antithesis: {
+  domain: "THET",
+  synonyms: ["direct opposite","reverse"]
+},
+
+epithet: {
+  domain: "THET",
+  synonyms: []
+},
+
+hypothesis: {
+  domain: "THET",
+  synonyms: []
+},
+
+synthesis: {
+  domain: "THET",
+  synonyms: ["combination"]
+},
+
+thesis: {
+  domain: "THET",
+  synonyms: ["proposition","statement","contention"]
+},
+
+parenthesis: {
+  domain: "THET",
+  synonyms: ["brackets"]
+},
+
+prosthetic: {
+  domain: "THET",
+  synonyms: []
+},
+
+polythetic: {
+  domain: "THET",
+  synonyms: []
+},
+aster: {
+  domain: "ASTER",
+  synonyms: []
+},
+
+asterisk: {
+  domain: "ASTER",
+  synonyms: []
+},
+
+asteroid: {
+  domain: "ASTER",
+  synonyms: []
+},
+
+astrologer: {
+  domain: "ASTER",
+  synonyms: []
+},
+
+astronaut: {
+  domain: "ASTER",
+  synonyms: ["spaceman"]
+},
+
+astronomer: {
+  domain: "ASTER",
+  synonyms: []
+},
+
+disaster: {
+  domain: "ASTER",
+  synonyms: ["misfortune","calamity"]
+},
+anagram: {
+  domain: "GRAM",
+  synonyms: []
+},
+
+cryptogram: {
+  domain: "GRAM",
+  synonyms: ["code"]
+},
+
+electrocardiogram: {
+  domain: "GRAM",
+  synonyms: []
+},
+
+epigram: {
+  domain: "GRAM",
+  synonyms: ["wit"]
+},
+
+graphic: {
+  domain: "GRAM",
+  synonyms: ["vivid","picturesque","symbolic","pictorial"]
+},
+
+graphite: {
+  domain: "GRAM",
+  synonyms: []
+},
+
+monogram: {
+  domain: "GRAM",
+  synonyms: []
+},
+
+monograph: {
+  domain: "GRAM",
+  synonyms: []
+},
+
+stenographer: {
+  domain: "GRAM",
+  synonyms: []
+},
+
+typography: {
+  domain: "GRAM",
+  synonyms: []
+}
+   
+  } 
+]
