043dfb6fc486c2f9a76a170b1ffd58da8ae57dff
zuonerut-stack <zuonerut@gmail.com>
2026-03-16 21:46:38 +0900

    Update similarity.js

--- data/similarity.js
+++ data/similarity.js
@@ -1,13 +1,207 @@
 const similarityData=[
 
 {
-sentence:"The two paintings are very similar.",
-target:"similar"
+sentence: "All school officials receive a salary except the members of the Board of Education, who serve without remuneration.",
+target: "remuneration",
+clue: "salary"
 },
 
 {
-sentence:"Her answer was almost identical to mine.",
-target:"identical"
+sentence: "Mrs. Joe has been out a dozen times, looking for you, Pip. And she's out now, making it a baker's dozen.",
+target: "a baker's dozen",
+clue: "dozen times + one"
+},
+
+{
+sentence: "I understand the first problem, but the second is beyond my comprehension.",
+target: "comprehension",
+clue: "understand"
+},
+
+{
+sentence: "In the marketplace of Goderville was a great crowd, a mingled multitude of men and beasts.",
+target: "multitude",
+clue: "crowd"
+},
+
+{
+sentence: "Jane's little brother has discovered the cache where she keeps her photographs. She'll have to find another hiding place.",
+target: "cache",
+clue: "hiding place"
+},
+
+{
+sentence: "Ellen tried her best to hold back her tears, but she couldn't restrain them.",
+target: "restrain",
+clue: "hold back"
+},
+
+{
+sentence: "Why are you so timorous? I tell you there is nothing to be afraid of.",
+target: "timorous",
+clue: "afraid of"
+},
+
+{
+sentence: "Our club's first president ran the meetings in such a despotic way that we called him 'the dictator'.",
+target: "despotic",
+clue: "dictator"
+},
+
+{
+sentence: "The Empire State Building is a remarkable edifice; it has more than a hundred stories.",
+target: "edifice",
+clue: "hundred stories"
+},
+
+{
+sentence: "Some children who are reserved with strangers are not at all uncommunicative with friends.",
+target: "uncommunicative",
+clue: "reserved"
+},
+
+{
+sentence: "Why should I retract my statement? It is a perfectly true remark and I see no reason to withdraw it.",
+target: "withdraw",
+clue: "retract"
+},
+
+{
+sentence: "Jerry thought he saw a ship in the distance. I looked carefully but could perceive nothing.",
+target: "perceive",
+clue: "saw"
+},
+
+{
+sentence: "Nina claims that I started the quarrel, but I have witnesses to prove that she initiated it.",
+target: "initiate",
+clue: "start"
+},
+
+{
+sentence: "He praised her taste and she commended his understanding.",
+target: "commend",
+clue: "praise"
+},
+
+{
+sentence: "Students attending private schools pay tuition. In the public schools, however, there is no charge for instruction.",
+target: "tuition",
+clue: "charge"
+},
+
+{
+sentence: "His facts no one thought of disputing; and his opinions few of the sailors dared to oppose.",
+target: "dispute",
+clue: "oppose"
+},
+
+{
+sentence: "Isn't it a pity that this beautiful edifice is going to be razed?",
+target: "raze",
+clue: "pity + beautiful edifice"
+},
+
+{
+sentence: "The boys were going to trade blows, but I managed to hold one of them back and my friend restrained the other.",
+target: "restrain",
+clue: "hold back"
+},
+
+{
+sentence: "When all at once I saw a crowd; a host of golden daffodils.",
+target: "a host of",
+clue: "a crowd"
+},
+
+{
+sentence: "Choosing a career is a matter that calls for reflection, but I haven't yet given it enough thought.",
+target: "reflection",
+clue: "give a thought to"
+},
+
+{
+sentence: "How can Alice tolerate your whistling while she is studying? I'd not bear it.",
+target: "tolerate",
+clue: "bear"
+},
+
+{
+sentence: "We shall have to convene somewhere else. We can't meet in the music room tomorrow because another group has reserved it.",
+target: "convene",
+clue: "meet"
+},
+
+{
+sentence: "I can dispense with a midmorning snack, but I cannot do without lunch.",
+target: "dispense with",
+clue: "do without"
+},
+
+{
+sentence: "Some pitchers try to intimidate batters by throwing fastballs very close to them, but they can't frighten a hitter like Joe.",
+target: "intimidate",
+clue: "frighten"
+},
+
+{
+sentence: "He soon found many of his former friends, though all rather the worse for the wear and tear of time; so Rip preferred making friends among the younger.",
+target: "cronies",
+clue: "friends"
+},
+
+{
+sentence: "The English Office is at one end of the hall and the library entrance is at the other extremity.",
+target: "extremity",
+clue: "at end of"
+},
+
+{
+sentence: "'Slow, lad, slow,' he said. Very deliberately then did we advance across the sand.",
+target: "deliberately",
+clue: "slow"
+},
+
+{
+sentence: "Two hours ago the weather bureau predicted rain for tomorrow; now it is forecasting rain mixed with snow.",
+target: "forecast",
+clue: "predict"
+},
+
+{
+sentence: "The old edition had a preface. The new one has no introduction at all.",
+target: "preface",
+clue: "introduction"
+},
+
+{
+sentence: "The dog would not relinquish it until she made him give it up.",
+target: "relinquish",
+clue: "give up"
+},
+
+{
+sentence: "I should always find that the calamities of life were shared among the upper and lower part of mankind; but that the middle station had the fewest disasters.",
+target: "calamities",
+clue: "disasters"
+},
+
+{
+sentence: "Yesterday it looked doubtful that I could finish my report on time. Today however, it seems less dubious.",
+target: "dubious",
+clue: "doubtful"
+},
+
+{
+sentence: "The low sales numbers are a reflection of our current strategy, which needs to be adjusted.",
+target: "reflection",
+clue: "needs to be adjusted"
+},
+
+{
+sentence: "Everyone makes a mistake once in a while; no one is infallible.",
+target: "infallible",
+clue: "make a mistake"
 }
 
 ]
