50e585aef42ab877f660f103e96e1d7d68bdb933
zuonerut-stack <zuonerut@gmail.com>
2026-03-17 00:49:01 +0900

    Update latin_prefix.js

--- data/latin_prefix.js
+++ data/latin_prefix.js
@@ -1,5 +1,857 @@
 window.LPData={
+  
+  "abdicate": {
+    domain: "ab",
+    synonyms: ["renounce","give up","resign"]
+  },
+  "abduct": {
+    domain: "ab",
+    synonyms: ["kidnap","snatch"]
+  },
+  "abhor": {
+    domain: "ab",
+    synonyms: ["shrink from","detest","loathe","hate"]
+  },
+  "abnormal": {
+    domain: "ab",
+    synonyms: ["deviating","unusual","irregular"]
+  },
+  "abrasion": {
+    domain: "ab",
+    synonyms: ["irritation"]
+  },
+  "abrupt": {
+    domain: "ab",
+    synonyms: ["broken off","sudden","unexpected"]
+  },
+  "abscond": {
+    domain: "ab",
+    synonyms: ["steal off","flee","escape"]
+  },
+  "averse to": {
+    domain: "ab",
+    synonyms: ["opposed to","disinclined to","unwilling to"]
+  },
+  "avocation": {
+    domain: "ab",
+    synonyms: ["hobby"]
+  },
+  "absolve": {
+    domain: "ab",
+    synonyms: ["exempt","excuse","exculpate","exonerate"]
+  },
+  "absorbing": {
+    domain: "ab",
+    synonyms: ["interesting","engrossing","fascinating","captivating"]
+  },
+  "abstain": {
+    domain: "ab",
+    synonyms: ["refrain","desist"]
+  },
+  "avert": {
+    domain: "ab",
+    synonyms: ["turn away","ward off","forestall"]
+  },
 
+  "adapt": {
+    domain: "ad",
+    synonyms: ["adjust","fit","suit","modify"]
+  },
+  "addicted": {
+    domain: "ad",
+    synonyms: ["habituated","devoted","dependent on"]
+  },
+  "adherent": {
+    domain: "ad",
+    synonyms: ["follower","supporter"]
+  },
+  "adjourn": {
+    domain: "ad",
+    synonyms: ["suspend","break off","defer","recess","retreat","withdraw"]
+  },
+  "advent": {
+    domain: "ad",
+    synonyms: ["arrival","approach","emergence"]
+  },
+  "adversary": {
+    domain: "ad",
+    synonyms: ["foe","antagonist","opponent","enemy"]
+  },
+  "adverse": {
+    domain: "ad",
+    synonyms: ["hostile","unfavorable","harmful"]
+  },
+  "adequate": {
+    domain: "ad",
+    synonyms: ["enough","sufficient","ample"]
+  },
+  "adjacent": {
+    domain: "ad",
+    synonyms: ["lying near","nearby","bordering","close"]
+  },
+  "adjoin": {
+    domain: "ad",
+    synonyms: ["be next to","be in contact with","border on","abut"]
+  },
 
+  "antecedents": {
+    domain: "ante",
+    synonyms: ["ancestors","forebears","predecessors"]
+  },
+  "antedate": {
+    domain: "ante",
+    synonyms: ["predate","precede"]
+  },
+  "ante meridiem": {
+    domain: "ante",
+    synonyms: []
+  },
+  "anteroom": {
+    domain: "ante",
+    synonyms: ["antechamber","waiting room"]
+  },
+
+  "postgraduate": {
+    domain: "post",
+    synonyms: ["graduate"]
+  },
+  "postmortem": {
+    domain: "post",
+    synonyms": ["autopsy"]
+  },
+  "postscript": {
+    domain: "post",
+    synonyms: []
+  },
+
+  "bicameral": {
+    domain: "bi",
+    synonyms: []
+  },
+  "bicentennial": {
+    domain: "bi",
+    synonyms: []
+  },
+  "biennial": {
+    domain: "bi",
+    synonyms: []
+  },
+  "bimonthly": {
+    domain: "bi",
+    synonyms: []
+  },
+  "bilateral": {
+    domain: "bi",
+    synonyms: ["two sided"]
+  },
+  "bilingual": {
+    domain: "bi",
+    synonyms: []
+  },
+  "bipartisan": {
+    domain: "bi",
+    synonyms: []
+  },
+  "bisect": {
+    domain: "bi",
+    synonyms: ["cut in half","halve"]
+  },
+
+  "semiannual": {
+    domain: "semi",
+    synonyms: []
+  },
+  "semimonthly": {
+    domain: "semi",
+    synonyms: []
+  },
+  "semicircle": {
+    domain: "semi",
+    synonyms: []
+  },
+  "semiconscious": {
+    domain: "semi",
+    synonyms: ["partially aware"]
+  },
+  "semidetached": {
+    domain: "semi",
+    synonyms: []
+  },
+  "semiskilled": {
+    domain: "semi",
+    synonyms: ["partly skilled"]
+  },
+  "semiconductor": {
+    domain: "semi",
+    synonyms: []
+  },
+
+  "emigrate": {
+    domain: "ex",
+    synonyms: []
+  },
+  "immigrate": {
+    domain: "in",
+    synonyms: []
+  },
+  "eminent": {
+    domain: "ex",
+    synonyms: ["conspicuous","distinguished","standing out"]
+  },
+  "imminent": {
+    domain: "in",
+    synonyms: ["about to occur","impending","threatening"]
+  },
+  "enervate": {
+    domain: "ex",
+    synonyms: ["weaken","enfeeble"]
+  },
+  "innervate": {
+    domain: "in",
+    synonyms: ["energize"]
+  },
+  "evoke": {
+    domain: "ex",
+    synonyms: ["bring out","elicit","produce"]
+  },
+  "invoke": {
+    domain: "in",
+    synonyms: ["call upon","implement"]
+  },
+  "excise": {
+    domain: "ex",
+    synonyms: ["cut out","remove"]
+  },
+  "incise": {
+    domain: "in",
+    synonyms: ["cut into","carve","engrave"]
+  },
+  "exhibit": {
+    domain: "ex",
+    synonyms: ["display","show"]
+  },
+  "inhibit": {
+    domain: "in",
+    synonyms: ["hold in","restrain"]
+  },
+  "explicate": {
+    domain: "ex",
+    synonyms: ["unfold","explain","analyze"]
+  },
+  "implicate": {
+    domain: "in",
+    synonyms: ["enfold","involve"]
+  },
+  "incarcerate": {
+    domain: "in",
+    synonyms: ["put in prison","confine"]
+  },
+  "inscribe": {
+    domain: "in",
+    synonyms: ["imprint","carve in","etch"]
+  },
+  "erode": {
+    domain: "ex",
+    synonyms: ["wear away","deteriorate","deplete"]
+  },
+  "exclusive": {
+    domain: "ex",
+    synonyms: ["single"]
+  },
+  "inclusive": {
+    domain: "in",
+    synonyms: ["comprehensive"]
+  },
+  "expel": {
+    domain: "ex",
+    synonyms: ["drive out","banish","eject","push out","remove","kick"]
+  },
+  "impel": {
+    domain: "in",
+    synonyms: ["drive on","force","compel","urge"]
+  },
+  "impugn": {
+    domain: "in",
+    synonyms: ["contradict","attack","malign","challenge"]
+  },
+  "insurgent": {
+    domain: "in",
+    synonyms: ["rebel","mutineer"]
+  },
+
+  "extracurricular": {
+    domain: "extra",
+    synonyms: []
+  },
+  "extraneous": {
+    domain: "extra",
+    synonyms: ["foreign","inessential","impertinent","irrelevant"]
+  },
+  "extravagant": {
+    domain: "extra",
+    synonyms: ["beyond the bounds","excessive","lavish"]
+  },
+
+  "intramural": {
+    domain: "intra",
+    synonyms: []
+  },
+  "intraparty": {
+    domain: "intra",
+    synonyms: []
+  },
+  "intrastate": {
+    domain: "intra",
+    synonyms: []
+  },
+  "intravenous": {
+    domain: "intra",
+    synonyms: []
+  },
+
+  "contraband": {
+    domain: "contra",
+    synonyms: []
+  },
+  "contravene": {
+    domain: "contra",
+    synonyms: ["go contrary to","violate","disregard"]
+  },
+  "controversy": {
+    domain: "contra",
+    synonyms: ["dispute","debate","quarrel"]
+  },
+  "counter to": {
+    domain: "counter",
+    synonyms: ["contrary"]
+  },
+  "countermand": {
+    domain: "counter",
+    synonyms: ["revoke","rescind","reverse"]
+  },
+  "incontrovertible": {
+    domain: "contra",
+    synonyms: ["unquestionable","certain"]
+  },
+
+  "intercede": {
+    domain: "inter",
+    synonyms: ["mediate","plead","intervene"]
+  },
+  "intercept": {
+    domain: "inter",
+    synonyms: ["interrupt","catch","seize"]
+  },
+  "interlinear": {
+    domain: "inter",
+    synonyms: []
+  },
+  "interlude": {
+    domain: "inter",
+    synonyms: ["interval","break","intermission"]
+  },
+  "intermediary": {
+    domain: "inter",
+    synonyms: ["go between","mediator"]
+  },
+  "intersect": {
+    domain: "inter",
+    synonyms: ["divide","cross","cut across"]
+  },
+  "interurban": {
+    domain: "inter",
+    synonyms: []
+  },
+  "intervene": {
+    domain: "inter",
+    synonyms: ["come between","intercede"]
+  },
+
+{
+  "illegible": {
+    domain: "il",
+    synonyms: ["undecipherable"]
+  },
+  "illiterate": {
+    domain: "il",
+    synonyms: ["uneducated"]
+  },
+  "illogical": {
+    domain: "il",
+    synonyms: ["irrational","fallacious"]
+  },
+  "illegal": {
+    domain: "il",
+    synonyms: ["illegitimate"]
+  },
+  "illicit": {
+    domain: "il",
+    synonyms: ["forbidden","unlawful"]
+  },
+  "illiquid": {
+    domain: "il",
+    synonyms: []
+  },
+
+  "immaculate": {
+    domain: "im",
+    synonyms: ["not spotted","clean","stainless"]
+  },
+  "immature": {
+    domain: "im",
+    synonyms: ["undeveloped","young","not grown"]
+  },
+  "impunity": {
+    domain: "im",
+    synonyms: []
+  },
+  "impatient": {
+    domain: "im",
+    synonyms: ["irritated","testy","restless","agitated","uptight"]
+  },
+  "immortal": {
+    domain: "im",
+    synonyms: ["undying","everlasting","eternal"]
+  },
+  "impolite": {
+    domain: "im",
+    synonyms: ["bad mannered","rude"]
+  },
+  "immobile": {
+    domain: "im",
+    synonyms: ["motionless","rigid","immobilized","fixed"]
+  },
+  "impartial": {
+    domain: "im",
+    synonyms: ["unbiased","fair","neutral","unprejudiced"]
+  },
+
+  "inaccessible": {
+    domain: "in",
+    synonyms: ["hard to get to","unreachable","hard approached"]
+  },
+  "incessant": {
+    domain: "in",
+    synonyms: ["interminable","ceaseless","pauseless"]
+  },
+  "inflexible": {
+    domain: "in",
+    synonyms: ["firm","unyielding","obstinate","unbent"]
+  },
+  "ingratitude": {
+    domain: "in",
+    synonyms: ["ungratefulness"]
+  },
+  "inhospitable": {
+    domain: "in",
+    synonyms: ["unfriendly","unwelcoming","uninviting"]
+  },
+  "insoluble": {
+    domain: "in",
+    synonyms: ["unsolvable"]
+  },
+  "inactive": {
+    domain: "in",
+    synonyms: ["idle","indolent","inoperative","nonfunctioning"]
+  },
+  "insecure": {
+    domain: "in",
+    synonyms: ["unprotected","unconfident"]
+  },
+  "incapable": {
+    domain: "in",
+    synonyms: ["unable to","incapacitated"]
+  },
+  "inaccurate": {
+    domain: "in",
+    synonyms: ["incorrect","inexact","imprecise"]
+  },
+
+  "irreconcilable": {
+    domain: "ir",
+    synonyms: ["incompatible"]
+  },
+  "irrelevant": {
+    domain: "ir",
+    synonyms: ["inapplicable","off topic","extraneous"]
+  },
+  "irrevocable": {
+    domain: "ir",
+    synonyms: ["unalterable","irreversible"]
+  },
+  "irresponsible": {
+    domain: "ir",
+    synonyms: ["reckless","careless"]
+  },
+  "irreplaceable": {
+    domain: "ir",
+    synonyms: ["unique","incomparable"]
+  },
+  "irreparable": {
+    domain: "ir",
+    synonyms: ["irretrievable"]
+  },
+
+  "beneficiary": {
+    domain: "bene",
+    synonyms: []
+  },
+  "benevolent": {
+    domain: "bene",
+    synonyms: ["kind","charitable","tolerant","nonprofit"]
+  },
+  "maladjusted": {
+    domain: "mal",
+    synonyms: []
+  },
+  "malice": {
+    domain: "mal",
+    synonyms: ["ill will","enmity","malevolence","hostility"]
+  },
+  "malnutrition": {
+    domain: "mal",
+    synonyms: ["poor nourishment"]
+  },
+  "maltreat": {
+    domain: "mal",
+    synonyms: ["mistreat","abuse","misuse"]
+  },
+
+  "decadent": {
+    domain: "de",
+    synonyms: ["deteriorating","growing worse","declining"]
+  },
+  "deciduous": {
+    domain: "de",
+    synonyms: []
+  },
+  "demolish": {
+    domain: "de",
+    synonyms: ["pull down","destroy","raze","wreck"]
+  },
+  "demote": {
+    domain: "de",
+    synonyms: ["degrade","declass"]
+  },
+  "dependent on": {
+    domain: "de",
+    synonyms: ["contingent on","determined by","conditional on"]
+  },
+  "depreciate": {
+    domain: "de",
+    synonyms: ["belittle","disparage","diminish"]
+  },
+  "devour": {
+    domain: "de",
+    synonyms: []
+  },
+  "demented": {
+    domain: "de",
+    synonyms: ["mad","insane","deranged"]
+  },
+  "despise": {
+    domain: "de",
+    synonyms: ["look down on","scorn","condemn","disdain"]
+  },
+  "deviate": {
+    domain: "de",
+    synonyms: ["turn aside from","stray","wander","digress"]
+  },
+
+  "discontent": {
+    domain: "dis",
+    synonyms: ["dissatisfied with"]
+  },
+  "discredit": {
+    domain: "dis",
+    synonyms: ["disbelieve","dishonor","disgrace"]
+  },
+  "discrepancy": {
+    domain: "dis",
+    synonyms: ["disagreement","inconsistency"]
+  },
+  "disrepair": {
+    domain: "dis",
+    synonyms: []
+  },
+  "dissent": {
+    domain: "dis",
+    synonyms: []
+  },
+  "dissident": {
+    domain: "dis",
+    synonyms: ["nonconformist"]
+  },
+  "dispassionate": {
+    domain: "dis",
+    synonyms: ["calm","impartial","composed"]
+  },
+  "distract": {
+    domain: "dis",
+    synonyms: ["divert","confuse","bewilder"]
+  },
+
+  "secede from": {
+    domain: "se",
+    synonyms: ["withdraw from"]
+  },
+  "seclude": {
+    domain: "se",
+    synonyms: ["isolate","sequester","keep away from"]
+  },
+  "secure": {
+    domain: "se",
+    synonyms: ["confident","assured","safe","fastened","tight","attached"]
+  },
+  "sedition": {
+    domain: "se",
+    synonyms: ["insurrection","treason","rebellion"]
+  },
+  "segregate": {
+    domain: "se",
+    synonyms: ["separate from","isolate"]
+  },
+
+  "circumference": {
+    domain: "circum",
+    synonyms: ["perimeter"]
+  },
+  "circumlocution": {
+    domain: "circum",
+    synonyms: ["tautology","roundabout speaking"]
+  },
+  "circumnavigate": {
+    domain: "circum",
+    synonyms: ["sail around"]
+  },
+  "circumscribe": {
+    domain: "circum",
+    synonyms: ["restrict","confine","draw around"]
+  },
+  "circumspect": {
+    domain: "circum",
+    synonyms: ["cautious","prudent","alert"]
+  },
+  "circumvent": {
+    domain: "circum",
+    synonyms: ["skirt","bypass","go around","overcome","sidestep"]
+  },
+
+  "coalesce": {
+    domain: "con",
+    synonyms: ["grow together","combine","unite"]
+  },
+  "coherent": {
+    domain: "con",
+    synonyms: ["logical","consistent"]
+  },
+  "collaborate": {
+    domain: "con",
+    synonyms: ["work together"]
+  },
+  "collusion": {
+    domain: "con",
+    synonyms: ["conspiracy","plot"]
+  },
+  "concord": {
+    domain: "con",
+    synonyms: ["harmony","agreement"]
+  },
+  "congenital": {
+    domain: "con",
+    synonyms: ["inborn","innate"]
+  },
+  "convene": {
+    domain: "con",
+    synonyms: ["meet","assemble","organize"]
+  },
+  "correspond": {
+    domain: "con",
+    synonyms: ["agree","match","tally","communicate"]
+  },
+
+  "obliterate": {
+    domain: "ob",
+    synonyms: ["erase","blot out","remove"]
+  },
+  "obsess": {
+    domain: "ob",
+    synonyms: ["preoccupy","trouble the mind of","haunt"]
+  },
+  "obstacle": {
+    domain: "ob",
+    synonyms: ["hindrance","impediment"]
+  },
+  "obstruct": {
+    domain: "ob",
+    synonyms: ["hinder","impede","block","clog","get in the way of"]
+  },
+  "obtrude": {
+    domain: "ob",
+    synonyms: ["intrude","impose"]
+  },
+  "obviate": {
+    domain: "ob",
+    synonyms: ["meet and dispose of","make unnecessary","forestall","avert"]
+  },
+
+  "perforate": {
+    domain: "per",
+    synonyms: ["pierce","puncture","punch"]
+  },
+  "perennial": {
+    domain: "per",
+    synonyms: ["enduring","unceasing"]
+  },
+  "permeate": {
+    domain: "per",
+    synonyms: ["pass through","penetrate","spread through","pervade","spread"]
+  },
+  "pertinent": {
+    domain: "per",
+    synonyms: ["applicable","relevant","apposite"]
+  },
+  "perturb": {
+    domain: "per",
+    synonyms: ["make uneasy","agitate","upset"]
+  },
+  "persist": {
+    domain: "per",
+    synonyms: ["continue","persevere","last","endure"]
+  },
+  "peruse": {
+    domain: "per",
+    synonyms: ["scrutinize","inspect","examine"]
+  },
+  "persevere": {
+    domain: "per",
+    synonyms: ["carry on","persist"]
+  },
+  "perform": {
+    domain: "per",
+    synonyms: ["carry out","accomplish","fulfill","present","stage"]
+  },
+  "perplex": {
+    domain: "per",
+    synonyms: ["puzzle","bewilder","embarass"]
+  },
+
+  "precede": {
+    domain: "pre",
+    synonyms: ["go before","come before"]
+  },
+  "preclude": {
+    domain: "pre",
+    synonyms: ["impede","prevent","prohibit"]
+  },
+  "precocious": {
+    domain: "pre",
+    synonyms: []
+  },
+  "preconceive": {
+    domain: "pre",
+    synonyms: ["predetermined","prejudiced"]
+  },
+  "prefabricated": {
+    domain: "pre",
+    synonyms: ["premanufactured"]
+  },
+  "preface": {
+    domain: "pre",
+    synonyms: ["foreword","preliminary remarks","introduction"]
+  },
+  "premature": {
+    domain: "pre",
+    synonyms: ["early","untimely","too soon"]
+  },
+  "premeditate": {
+    domain: "pre",
+    synonyms: ["preconsider"]
+  },
+  "presume": {
+    domain: "pre",
+    synonyms: ["assume","suppose","hypothesize"]
+  },
+  "preview": {
+    domain: "pre",
+    synonyms: []
+  },
+  "prefix": {
+    domain: "pre",
+    synonyms: []
+  },
+  "president": {
+    domain: "pre",
+    synonyms: ["head","chief","leader"]
+  },
+  "prefrontal": {
+    domain: "pre",
+    synonyms: ["foremost"]
+  },
+  "predicate": {
+    domain: "pre",
+    synonyms: ["base on"]
+  },
+  "preach": {
+    domain: "pre",
+    synonyms: ["proclaim","advocate"]
+  },
+  "prefer": {
+    domain: "pre",
+    synonyms: ["favor","submit"]
+  },
+  "precipitous": {
+    domain: "pre",
+    synonyms: ["steep","high","hasty","rash","hurried"]
+  },
+
+  "procrastinate": {
+    domain: "pro",
+    synonyms: ["postpone","delay","dawdle"]
+  },
+  "proficient": {
+    domain: "pro",
+    synonyms: ["skilled","adept","expert"]
+  },
+  "profuse": {
+    domain: "pro",
+    synonyms: ["extravagant","lavish","copious"]
+  },
+  "project": {
+    domain: "pro",
+    synonyms: ["throw","cast forward","hurl","protrude","stick out"]
+  },
+  "prominent": {
+    domain: "pro",
+    synonyms: ["well known","important","famous","protruding","projecting"]
+  },
+  "propel": {
+    domain: "pro",
+    synonyms: ["impel forward","drive onward","force ahead","push","thrust"]
+  },
+  "proponent": {
+    domain: "pro",
+    synonyms: ["advocate","supporter"]
+  },
+  "prospects": {
+    domain: "pro",
+    synonyms: ["chances","opportunities","likelihoods"]
+  },
+  "protract": {
+    domain: "pro",
+    synonyms: ["draw out","lengthen","prolong"]
+  },
+  "protrude": {
+    domain: "pro",
+    synonyms: ["thrust forth","stick out","bulge","jut"]
+  },
+  "provoke": {
+    domain: "pro",
+    synonyms: ["evoke","irritate","call forth","bring on"]
+  }
 
 }
+
+
+
+
