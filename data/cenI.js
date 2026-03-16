775ce5b6e95fdd5f485e1d5eb151079f8cfc20d5
zuonerut-stack <zuonerut@gmail.com>
2026-03-17 00:15:57 +0900

    Update cenI.js

--- data/cenI.js
+++ data/cenI.js
@@ -1,1 +1,751 @@
+window.cenData=[
+adroit: {
+  domain: "Skill",
+  synonyms: ["skillful","clever","deft"]
+},
 
+ambidextrous: {
+  domain: "Skill",
+  synonyms: []
+},
+
+apprentice: {
+  domain: "Skill",
+  synonyms: ["learner","novice","tyro"]
+},
+
+aptitude: {
+  domain: "Skill",
+  synonyms: ["bent","talent"]
+},
+
+craftsperson: {
+  domain: "Skill",
+  synonyms: ["artisan"]
+},
+
+dexterity: {
+  domain: "Skill",
+  synonyms: ["deftness","expertise"]
+},
+
+maladroit: {
+  domain: "Skill",
+  synonyms: ["clumsy","inept","awkward"]
+},
+
+versatile: {
+  domain: "Skill",
+  synonyms: ["many-sided","all-around"]
+},
+
+destitute: {
+  domain: "Poverty",
+  synonyms: ["needy","indigent"]
+},
+
+economize: {
+  domain: "Poverty",
+  synonyms: ["save"]
+},
+
+frugal: {
+  domain: "Poverty",
+  synonyms: ["scanty","saving","thrifty"]
+},
+
+impoverish: {
+  domain: "Poverty",
+  synonyms: ["bankrupt","ruin","make poor"]
+},
+
+indigence: {
+  domain: "Poverty",
+  synonyms: ["poverty","penury"]
+}
+
+affluent: {
+  domain: "Wealth",
+  synonyms: ["rich","wealthy","opulent"]
+},
+
+avarice: {
+  domain: "Wealth",
+  synonyms: ["greediness","cupidity"]
+},
+
+avaricious: {
+  domain: "Wealth",
+  synonyms: ["greedy","grasping","covetous"]
+},
+
+covet: {
+  domain: "Wealth",
+  synonyms: ["desire","long for","crave"]
+},
+
+dowry: {
+  domain: "Wealth",
+  synonyms: []
+},
+
+financial: {
+  domain: "Wealth",
+  synonyms: ["monetary","pecuniary","fiscal"]
+},
+
+fleece: {
+  domain: "Wealth",
+  synonyms: ["rob","swindle","cheat"]
+},
+
+hoard: {
+  domain: "Wealth",
+  synonyms: ["accumulate","amass"]
+},
+
+lavish: {
+  domain: "Wealth",
+  synonyms: ["profuse","extravagant"]
+},
+
+lucrative: {
+  domain: "Wealth",
+  synonyms: ["profitable","moneymaking"]
+},
+
+means: {
+  domain: "Wealth",
+  synonyms: ["wealth","property","resources"]
+},
+
+opulence: {
+  domain: "Wealth",
+  synonyms: ["riches","affluence"]
+},
+
+sumptuous: {
+  domain: "Wealth",
+  synonyms: ["luxurious","costly"]
+},
+
+apprehensive: {
+  domain: "Fear",
+  synonyms: ["afraid","anxious"]
+},
+
+cower: {
+  domain: "Fear",
+  synonyms: ["cringe","recoil"]
+},
+
+dastardly: {
+  domain: "Fear",
+  synonyms: ["cowardly","mean"]
+},
+
+intimidate: {
+  domain: "Fear",
+  synonyms: ["frighten","cow","bully"]
+},
+
+poltroon: {
+  domain: "Fear",
+  synonyms: ["coward","dastard","craven"]
+},
+
+timid: {
+  domain: "Fear",
+  synonyms: ["fearful","timorous","shy"]
+},
+
+trepidation: {
+  domain: "Fear",
+  synonyms: ["fear","fright","trembling"]
+},
+
+audacious: {
+  domain: "Courage",
+  synonyms: ["bold","daring","insolent","impudent"]
+},
+
+audacity: {
+  domain: "Courage",
+  synonyms: ["nerve","rashness","temerity"]
+},
+
+dauntless: {
+  domain: "Courage",
+  synonyms: ["fearless","intrepid","valiant"]
+},
+
+exploit: {
+  domain: "Courage",
+  synonyms: ["feat","deed","act"]
+},
+
+fortitude: {
+  domain: "Courage",
+  synonyms: ["endurance","bravery","pluck","backbone","valor"]
+},
+
+indomitable: {
+  domain: "Courage",
+  synonyms: ["unconquerable","invincible"]
+},
+
+plucky: {
+  domain: "Courage",
+  synonyms: ["courageous","brave","valiant"]
+},
+
+rash: {
+  domain: "Courage",
+  synonyms: ["overhasty","foolhardy","reckless","impetuous"]
+},
+
+colossal: {
+  domain: "Size_Quantity",
+  synonyms: ["huge","enormous","vast"]
+},
+
+commodious: {
+  domain: "Size_Quantity",
+  synonyms: ["spacious","roomy","ample"]
+},
+
+gamut: {
+  domain: "Size_Quantity",
+  synonyms: ["range","span"]
+},
+
+infinite: {
+  domain: "Size_Quantity",
+  synonyms: ["boundless","inexhaustible"]
+},
+
+infinitesimal: {
+  domain: "Size_Quantity",
+  synonyms: []
+},
+
+inflate: {
+  domain: "Size_Quantity",
+  synonyms: ["expand","puff up"]
+},
+
+inordinate: {
+  domain: "Size_Quantity",
+  synonyms: ["excessive","immoderate"]
+},
+
+iota: {
+  domain: "Size_Quantity",
+  synonyms: ["infinitesimal amount","a bit"]
+},
+
+magnitude: {
+  domain: "Size_Quantity",
+  synonyms: ["size","greatness","importance"]
+},
+
+picayune: {
+  domain: "Size_Quantity",
+  synonyms: ["petty","small"]
+},
+
+pittance: {
+  domain: "Size_Quantity",
+  synonyms: []
+},
+
+puny: {
+  domain: "Size_Quantity",
+  synonyms: ["weak","insignificant"]
+},
+
+superabundance: {
+  domain: "Size_Quantity",
+  synonyms: ["surplus","excess"]
+},
+
+debilitate: {
+  domain: "Weakness",
+  synonyms: ["enfeeble","weaken"]
+},
+
+decadent: {
+  domain: "Weakness",
+  synonyms: ["falling off","declining","deteriorating"]
+},
+
+decrepit: {
+  domain: "Weakness",
+  synonyms: ["worn out"]
+},
+
+dilapidated: {
+  domain: "Weakness",
+  synonyms: ["decayed","ruined","neglected"]
+},
+
+enervate: {
+  domain: "Weakness",
+  synonyms: ["weaken","enfeeble"]
+},
+
+flimsy: {
+  domain: "Weakness",
+  synonyms: ["frail","unsubstantial"]
+},
+
+frail: {
+  domain: "Weakness",
+  synonyms: ["fragile","weak"]
+},
+
+incapacitate: {
+  domain: "Weakness",
+  synonyms: ["disable","paralyze"]
+},
+
+infirmity: {
+  domain: "Weakness",
+  synonyms: ["feebleness","frailty"]
+},
+
+debilitate: {
+  domain: "Weakness",
+  synonyms: ["enfeeble","weaken"]
+},
+
+decadent: {
+  domain: "Weakness",
+  synonyms: ["falling off","declining","deteriorating"]
+},
+
+decrepit: {
+  domain: "Weakness",
+  synonyms: ["worn out"]
+},
+
+dilapidated: {
+  domain: "Weakness",
+  synonyms: ["decayed","ruined","neglected"]
+},
+
+enervate: {
+  domain: "Weakness",
+  synonyms: ["weaken","enfeeble"]
+},
+
+flimsy: {
+  domain: "Weakness",
+  synonyms: ["frail","unsubstantial"]
+},
+
+frail: {
+  domain: "Weakness",
+  synonyms: ["fragile","weak"]
+},
+
+incapacitate: {
+  domain: "Weakness",
+  synonyms: ["disable","paralyze"]
+},
+
+infirmity: {
+  domain: "Weakness",
+  synonyms: ["feebleness","frailty"]
+},
+
+bulwark: {
+  domain: "Strength",
+  synonyms: ["rampart","defense"]
+},
+
+citadel: {
+  domain: "Strength",
+  synonyms: ["fortress","stronghold"]
+},
+
+cogent: {
+  domain: "Strength",
+  synonyms: ["forcible","compelling","convincing"]
+},
+
+dynamic: {
+  domain: "Strength",
+  synonyms: ["forceful","energetic","active"]
+},
+
+formidable: {
+  domain: "Strength",
+  synonyms: ["dreadful","fear-inspiring"]
+},
+
+forte: {
+  domain: "Strength",
+  synonyms: ["strong point","speciality"]
+},
+
+impregnable: {
+  domain: "Strength",
+  synonyms: ["unconquerable","invincible"]
+},
+
+invigorate: {
+  domain: "Strength",
+  synonyms: ["strengthen","enliven"]
+},
+
+robust: {
+  domain: "Strength",
+  synonyms: ["vigorous","sturdy","sound"]
+},
+
+tenacious: {
+  domain: "Strength",
+  synonyms: ["fast-holding","unyielding","stubborn"]
+},
+
+vehement: {
+  domain: "Strength",
+  synonyms: ["forceful","violent","furious"]
+},
+
+vigor: {
+  domain: "Strength",
+  synonyms: ["activeness","energy"]
+},
+
+default: {
+  domain: "Neglect",
+  synonyms: ["negligence","neglect"]
+},
+
+heedless: {
+  domain: "Neglect",
+  synonyms: ["inattentive","careless","unmindful","reckless"]
+},
+
+ignore: {
+  domain: "Neglect",
+  synonyms: ["disregard","overlook"]
+},
+
+inadvertent: {
+  domain: "Neglect",
+  synonyms: ["heedless","thoughtless"]
+},
+
+disregard: {
+  domain: "Neglect",
+  synonyms: ["neglect"]
+},
+
+remiss: {
+  domain: "Neglect",
+  synonyms: ["negligent","lax","slack-off"]
+},
+
+sloven: {
+  domain: "Neglect",
+  synonyms: ["untidy person"]
+},
+
+discreet: {
+  domain: "Care",
+  synonyms: ["cautious","circumspect"]
+},
+
+heed: {
+  domain: "Care",
+  synonyms: ["take notice of","give attention to","mind about"]
+},
+
+meticulous: {
+  domain: "Care",
+  synonyms: ["fussy","diligent","conscientious"]
+},
+
+scrupulous: {
+  domain: "Care",
+  synonyms: ["honest","strict","precise"]
+},
+
+scrutinize: {
+  domain: "Care",
+  synonyms: ["examine","inspect"]
+},
+
+solicitude: {
+  domain: "Care",
+  synonyms: ["excessive care","anxiety"]
+},
+
+vigilance: {
+  domain: "Care",
+  synonyms: ["alertness","caution","watchfulness"]
+},
+
+wary: {
+  domain: "Care",
+  synonyms: ["cautious","vigilant"]
+},
+
+
+abroad: {
+  domain: "Residence",
+  synonyms: ["overseas","widely"]
+},
+
+commute: {
+  domain: "Residence",
+  synonyms: []
+},
+
+denizen: {
+  domain: "Residence",
+  synonyms: ["inhabitant","occupant"]
+},
+
+domicile: {
+  domain: "Residence",
+  synonyms: ["house","dwelling","abode"]
+},
+
+inmate: {
+  domain: "Residence",
+  synonyms: ["patient","prisoner"]
+},
+
+migrate: {
+  domain: "Residence",
+  synonyms: ["relocate","resettle","roam"]
+},
+
+native: {
+  domain: "Residence",
+  synonyms: []
+},
+
+nomad: {
+  domain: "Residence",
+  synonyms: ["wanderer","wayfarer"]
+},
+
+sojourn: {
+  domain: "Residence",
+  synonyms: ["stay","visit","stopover"]
+},
+
+
+defiance: {
+  domain: "Disobedience",
+  synonyms: ["refusal","resistance"]
+},
+
+infraction: {
+  domain: "Disobedience",
+  synonyms: ["violation","breach"]
+},
+
+insubordinate: {
+  domain: "Disobedience",
+  synonyms: ["disobedient","mutinous","rebellious"]
+},
+
+insurgent: {
+  domain: "Disobedience",
+  synonyms: ["rebel","revolutionary"]
+},
+
+malcontent: {
+  domain: "Disobedience",
+  synonyms: []
+},
+
+perverse: {
+  domain: "Disobedience",
+  synonyms: ["obstinate","willful","wayward"]
+},
+
+sedition: {
+  domain: "Disobedience",
+  synonyms: ["treason"]
+},
+
+transgress: {
+  domain: "Disobedience",
+  synonyms: ["infringe","go beyond"]
+},
+
+trespass: {
+  domain: "Disobedience",
+  synonyms: ["encroach","intrude on"]
+},
+
+acquiesce: {
+  domain: "Obedience",
+  synonyms: ["comply","permit"]
+},
+
+allegiance: {
+  domain: "Obedience",
+  synonyms: ["loyalty","devotion","fidelity"]
+},
+
+defer: {
+  domain: "Obedience",
+  synonyms: ["yield to","submit"]
+},
+
+discipline: {
+  domain: "Obedience",
+  synonyms: ["control","train"]
+},
+
+docile: {
+  domain: "Obedience",
+  synonyms: ["obedient","tractable","submissive"]
+},
+
+meek: {
+  domain: "Obedience",
+  synonyms: ["submissive","acquiescent"]
+},
+
+pliable: {
+  domain: "Obedience",
+  synonyms: ["easy-bent","yielding","adaptable"]
+},
+
+submit: {
+  domain: "Obedience",
+  synonyms: ["yield","surrender"]
+},
+
+tractable: {
+  domain: "Obedience",
+  synonyms: ["docile"]
+},
+
+chronic: {
+  domain: "Time",
+  synonyms: ["longing","recurring","persistent"]
+},
+
+concurrent: {
+  domain: "Time",
+  synonyms: ["simultaneous","contemporary"]
+},
+
+dawdle: {
+  domain: "Time",
+  synonyms: ["waste","loiter","idle"]
+},
+
+imminent: {
+  domain: "Time",
+  synonyms: ["impending","at hand","approaching"]
+},
+
+incipient: {
+  domain: "Time",
+  synonyms: ["commencing","initial"]
+},
+
+intermittent: {
+  domain: "Time",
+  synonyms: ["recurrent","periodic"]
+},
+
+perennial: {
+  domain: "Time",
+  synonyms: ["incessant","enduring","perpetual"]
+},
+
+procrastinate: {
+  domain: "Time",
+  synonyms: ["defer","postpone"]
+},
+
+protract: {
+  domain: "Time",
+  synonyms: ["draw out","lengthen","prolong","extend"]
+},
+
+sporadic: {
+  domain: "Time",
+  synonyms: ["isolated","infrequent"]
+},
+
+
+compulsory: {
+  domain: "Necessity",
+  synonyms: ["obligatory","mandatory","requisite"]
+},
+
+entail: {
+  domain: "Necessity",
+  synonyms: ["impose","require","necessitate"]
+},
+
+essence: {
+  domain: "Necessity",
+  synonyms: ["fundamental nature","core","spirit"]
+},
+
+gratuitous: {
+  domain: "Necessity",
+  synonyms: ["unwanted","uncalled for","unwarranted"]
+},
+
+imperative: {
+  domain: "Necessity",
+  synonyms: ["urgent","necessary","compulsory"]
+},
+
+"incumbent on": {
+  domain: "Necessity",
+  synonyms: ["obligatory","binding"]
+},
+
+indispensable: {
+  domain: "Necessity",
+  synonyms: ["essential","crucial","vital"]
+},
+
+necessitate: {
+  domain: "Necessity",
+  synonyms: ["require","demand"]
+},
+
+oblige: {
+  domain: "Necessity",
+  synonyms: ["compel","force","put under obligation"]
+},
+
+obviate: {
+  domain: "Necessity",
+  synonyms: ["preclude","make unnecessary","get rid of"]
+},
+
+prerequisite: {
+  domain: "Necessity",
+  synonyms: ["precondition"]
+},
+
+pressing: {
+  domain: "Necessity",
+  synonyms: ["urgent"]
+},
+
+superfluous: {
+  domain: "Necessity",
+  synonyms: ["surplus","excessive"]
+}
+
+
+]
